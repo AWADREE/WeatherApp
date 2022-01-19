a438b494f24f315b4a8e2554b9c2c6c3402db936
AWADREE <62517784+AWADREE@users.noreply.github.com>
2022-01-19 16:16:56 +0200

    added more styling

--- wa_frontend/src/components/Header.js
+++ wa_frontend/src/components/Header.js
@@ -1,9 +1,13 @@
 import "./Header.css";
+import NightsStayIcon from "@material-ui/icons/NightsStay";
 
 const Header = () => {
   return (
     <header className="header-style">
-      <div>Merry Weather</div>
+      <div>
+        Merry Weather
+        <NightsStayIcon className="icon" />
+      </div>
     </header>
   );
 };
